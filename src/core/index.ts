95035d553b3a54e0a686b654bf17ef4beb06325e
Miquel <miquellopezavila@gmail.com>
2022-09-11 13:23:25 +0200

    Second commit

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,6 @@
-import useNavigation from 'navigator/Navigator';
-import { types } from 'common/enums/types';
-import { keyCodeKeyBoard, keyCodeWheel } from 'common/enums/key-codes';
+import useNavigation from '../navigator/Navigator';
+import { types } from '../common/enums/types';
+import { keyCodeKeyBoard, keyCodeWheel } from '../common/enums/key-codes';
 
 export {
   types,
